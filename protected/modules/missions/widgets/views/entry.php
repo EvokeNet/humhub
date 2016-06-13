5376445b4368f554f8e7a899f2c2703220bde33a
guilherme_fernandes <gfernandes@quanti.ca>
2016-06-13 17:55:05 -0300

    evidence review - html started

--- protected/modules/missions/widgets/views/entry.php
+++ protected/modules/missions/widgets/views/entry.php
@@ -25,6 +25,45 @@ $activity = $evidence->getActivities();
 
 <?php echo Html::endForm(); ?>
 
+<BR>
+<div class="panel-group">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" href="#collapse1">Review</a>
+      </h4>
+    </div>
+    <div id="collapse1" class="panel-collapse collapse">
+    	<h2>Distribute points for Curiosity</h2>
+    	<p>
+    		Here is  the question that confirms whether or not the evidence is sufficient.
+    		Here is  the question that confirms whether or not the evidence is sufficient.
+    		Here is  the question that confirms whether or not the evidence is sufficient.
+    	</p>
+    	<div>
+    		<div class="radio">
+			<label>
+				<input type="radio" name="optradio">
+					Yes
+				</label>
+				<div>
+					<label class="radio-inline"><input type="radio" name="optradio">1</label>
+					<label class="radio-inline"><input type="radio" name="optradio">2</label>
+					<label class="radio-inline"><input type="radio" name="optradio">3</label>
+				</div>
+			</div>
+			<div class="radio">
+			<label>
+				<input type="radio" name="optradio">
+					No
+				</label>
+			</div>
+    	</div>
+    </div>
+  </div>
+</div>
+
+
 <style type="text/css">
 
 .activity_area{
--- protected/modules/missions/widgets/views/form.php
+++ protected/modules/missions/widgets/views/form.php
@@ -52,7 +52,6 @@ $firstSecondary = true;
     echo Html::textArea("title", '', array('id' => 'contentForm_question', 'class' => 'form-control autosize contentForm', 'rows' => '1', "tabindex" => "1", 'placeholder' => Yii::t('MissionsModule.widgets_views_evidenceForm', "Page Title"))); 
     echo Html::textArea("text", '', array('id' => 'contentForm_question', 'class' => 'text-margin form-control autosize contentForm', 'rows' => '10', "tabindex" => "2", 'placeholder' => Yii::t('MissionsModule.widgets_views_evidenceForm', "Content")));
 
-
 ?>
 
 <style type="text/css">
