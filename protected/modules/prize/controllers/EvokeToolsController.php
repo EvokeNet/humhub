cda6a7117cdcd03719521a3d37bede083e8c9d1c
Jayant Gandhi <jayant.gandhi127@gmail.com>
2016-07-15 03:12:14 -0400

    bulk of random chance code (needs fine tuning)

--- protected/modules/prize/controllers/EvokeToolsController.php
+++ protected/modules/prize/controllers/EvokeToolsController.php
@@ -6,6 +6,8 @@ use Yii;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use app\modules\prize\models\Prize;
+use app\modules\prize\models\WonPrize;
+use humhub\modules\user\models\User;
 
 /**
  * Evoke Tools Controller
@@ -13,6 +15,7 @@ use app\modules\prize\models\Prize;
  */
 class EvokeToolsController extends Controller
 {
+    public $max_prob = 10000;
 
     public function actionIndex()
     {
@@ -23,9 +26,49 @@ class EvokeToolsController extends Controller
 
     public function actionSearch()
     {
-      $prizes = Prize::find()->all();
-      $roll = mt_rand(0, 1000);
-      $results = $roll;
+      $prizes = Prize::find()->where('quantity' > 0)->all();
+      $roll = mt_rand(0, $this->max_prob);
+      $probabilities = [];
+      $available_prizes = [];
+      $prize_won = '';
+
+      // workout the probabilities for each prize
+      // this is tied to when the prize was set to be available
+      // i.e. if the prize has been around for a while it is more likely to come up
+      foreach ($prizes as $prize) {
+        $date = $prize->week_of;
+        // see how long the prize has been available - affects the probability of winning it
+        $time_diff = date_diff(new \DateTime($date), new \DateTime());
+        $diff = (int)$time_diff->format('%R%a'); // int of days
+
+        if ($diff >= 0) { //prize not available yet if diff < 0
+          $prize_prob = $prize->weight * ($diff + 1);
+          $probabilities[] = $prize_prob;
+          $available_prizes[] = $prize;
+        }
+      }
+
+      // figure out if they won anything
+      // done by counting down from the max prob and seeing if they scored a number above it
+      $prob_checker = $this->max_prob; //here to keep track of where we are in the number line
+      foreach ($probabilities as $key => $probability) {
+        $prob_checker -= $probability;
+
+        if ($roll >= $prob_checker) { // they won!
+          $prize_won = $available_prizes[$key];
+          $available_prizes[$key]->quantity -= 1; // minus a quantity from the prize
+          $available_prizes[$key]->save();
+
+          $won_prize = new WonPrize();
+          $won_prize->prize_id = $prize_won->id;
+          $won_prize->user_id = $app->user->id;
+          $won_prize->save();
+
+          break;
+        }
+      }
+
+      $results = '<div>' . $roll . '</div><strong>' . $date . '</strong><div>' . $diff . '</div><div>' . implode(',', $probabilities);
 
       return $this->render('evoke_tools/index', array('prizes' => $prizes, 'results' => $results));
     }
---
+++ protected/modules/prize/models/WonPrize.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace app\modules\prize\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+use yii\db\Expression;
+use yii\behaviors\TimestampBehavior;
+use humhub\modules\user\models\User;
+
+/**
+ * This is the model class for table `wallet`
+ *
+ * @property integer  $id
+ * @property integer  $user_id
+ * @property integer  $prize_id
+
+ */
+ class WonPrize extends \yii\db\ActiveRecord
+ {
+   public function behaviors()
+   {
+     //custom behaviors go here
+   }
+
+   /**
+    * @inheritdoc
+    */
+    public static function tablename()
+    {
+      return 'won_prizes';
+    }
+
+    /**
+     * @inheritdoc
+     */
+     public function rules()
+     {
+       return [
+         [['user_id', 'prize_id'], 'required'],
+         [['user_id', 'prize_id'], 'integer'],
+         [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+         [['prize_id'], 'exist', 'skipOnError' => true, 'targetClass' => Coin::className(), 'targetAttribute' => ['prize_id' => 'id']],
+       ];
+     }
+
+     /**
+      * @inheritdoc
+      */
+      public function attributeLabels()
+      {
+        return [
+          'id' => Yii::t('CoinModule.base', 'ID'),
+          'user_id' => Yii::t('CoinModule.base', 'Owner ID'),
+          'prize_id' => Yii::t('CoinModule.base', 'Coin ID'),
+        ];
+      }
+
+ }
