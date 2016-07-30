5be00719317852c4fb7eb93b7fa363ea31fb8532
Renata Japur <rjapur@quanti.ca>
2016-07-30 00:13:56 -0300

    space pic on front of user pic fix

--- themes/Evoke/views/content/widgets/wallEntry.php
+++ themes/Evoke/views/content/widgets/wallEntry.php
@@ -43,17 +43,19 @@ if($team_id){
 
             <!-- Show space image, if you are outside from a space -->
             <?php if (!Yii::$app->controller instanceof ContentContainerController && $object->content->container instanceof Space): ?>
-                <?php echo \humhub\modules\space\widgets\Image::widget([
-                    'space' => $object->content->container,
-                    'width' => 20,
-                    'htmlOptions' => [
-                        'class' => 'img-space',
-                    ],
-                    'link' => 'true',
-                    'linkOptions' => [
-                        'class' => 'pull-left',
-                    ],
-                ]); ?>
+                <?php 
+                // echo \humhub\modules\space\widgets\Image::widget([
+                //     'space' => $object->content->container,
+                //     'width' => 20,
+                //     'htmlOptions' => [
+                //         'class' => 'img-space',
+                //     ],
+                //     'link' => 'true',
+                //     'linkOptions' => [
+                //         'class' => 'pull-left',
+                //     ],
+                // ]); 
+                ?>
 
             <?php endif; ?>
 
