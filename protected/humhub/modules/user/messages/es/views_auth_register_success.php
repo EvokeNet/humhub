fed77e01eab2b43290cf88246c748ebbb0efb378
Renata Japur <rjapur@quanti.ca>
2016-08-09 07:57:01 -0300

    spanish sign up email and create account

--- protected/humhub/modules/user/messages/es/views_auth_register_success.php
+++ protected/humhub/modules/user/messages/es/views_auth_register_success.php
@@ -17,8 +17,7 @@
  * NOTE: this file must be saved in UTF-8 encoding.
  */
 return [
-    'Registration successful' => '
-Registro exitoso',
+    'Registration successful' => 'Registro exitoso',
     '<strong>Registration</strong> successful!' => '¡<strong>Registrado</strong> con éxito!',
     'Please check your email and follow the instructions!' => 'Por favor, comprueba tu correo electrónico y sigue las instrucciones.',
     'back to home' => 'Volver a Inicio',
--- protected/humhub/modules/user/messages/es/views_mails_UserInviteSelf.php
+++ protected/humhub/modules/user/messages/es/views_mails_UserInviteSelf.php
@@ -2,6 +2,8 @@
 return array (
   'Registration Link' => 'Enlace de registro',
   'Sign up' => 'Registrarse',
+  'Sign Up' => 'Registrarse',
+  'Please click on the button below to proceed with your registration!' => 'Por favor, clica en el botón para continuar con su registro!',
   'Welcome to %appName%' => 'Bienvenido a %appName%',
   'Welcome to %appName%. Please click on the button below to proceed with your registration.' => 'Bienvenido a %appName%.Por favor, haz clic en el botón de abajo para proceder a registrarte.',
 );
