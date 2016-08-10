b32d46b67ce8e6107a433061dcb1cf9d17c13724
Renata Japur <rjapur@quanti.ca>
2016-08-09 22:23:19 -0300

    mail content fix

--- protected/humhub/modules/user/messages/es/views_mails_UserInviteSelf.php
+++ protected/humhub/modules/user/messages/es/views_mails_UserInviteSelf.php
@@ -1,9 +1,11 @@
 <?php
 return array (
   'Registration Link' => 'Enlace de registro',
-  'Sign up' => 'Registrarse',
-  'Sign Up' => 'Registrarse',
+  'Sign up' => 'Regístrate aquí',
+  'Sign Up' => 'Regístrate aquí',
   'Please click on the button below to proceed with your registration!' => 'Por favor, clica en el botón para continuar con su registro!',
   'Welcome to %appName%' => 'Bienvenido a %appName%',
   'Welcome to %appName%. Please click on the button below to proceed with your registration.' => 'Bienvenido a %appName%.Por favor, haz clic en el botón de abajo para proceder a registrarte.',
+  'This is Alchemy, and this is an Urgent Evoke. Wherever you are, whoever you are, if you found this message, it\'s your destiny to join us.' => 'Soy Alchemy, y este es un Evoke urgente. No importa dónde estás o quién eres, si encontraste este mensaje, tu destino es unirte a nosotros.',
+  'Evoke is a network of social innovators that use their powers to save the world. And this is an invitation to join us.<br><br>During the next 16 weeks you and your team will complete 8 missions and create with the community an Evokation -- your team project that seeks to solve problems in your community of Soacha.  At the end of this journey, the Evoke network will recognize the best Evokations.<br><br>Evoke is counting on you' => 'Evoke es la red de innovadores sociales que usan sus poderes para salvar el mundo. Y esta es una invitación para que hagas parte de ella.<br><br>Durante las siguientes 16 semanas tú y tu equipo completarán 8 misiones y crearán con la comunidad una “evokación”, un proyecto personal que busca solucionar algunas de las problemáticas en Soacha. Al final, la Red Evoke reconocerá las mejores evokaciones.<br><br>Evoke cuenta contigo.',
 );
