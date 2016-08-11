5929d6711c6ece8d02537475a34be35553425682
guilherme_fernandes <gfernandes@quanti.ca>
2016-08-11 17:09:44 -0300

    Update Create Agent

--- protected/modules/missions/widgets/views/create_a_team.php
+++ protected/modules/missions/widgets/views/create_a_team.php
@@ -14,6 +14,31 @@ use yii\helpers\Url;
     <div class="panel-body">
         <div class="col-xs-12">
             <!-- content -->
+            <!--English-->
+            <b class="uppercase">Team captains</b>
+            <p>
+                Your first mission is 
+                <a href="<?= Url::to(['/teams/create/create_team']) ?>" data-target = "#globalModal">
+                    register your team on the platform.
+                </a>
+            </p>
+            <b class="uppercase">Other agents</b>
+            <p>
+                Await the invitation from your captain to join your team.
+            </p>
+
+            <!--Spanish-->
+            <b class="uppercase">Agentes capitanes:</b> 
+            <p>
+                Su primera misión es registrar el
+                <a href="<?= Url::to(['/teams/create/create_team']) ?>" data-target = "#globalModal">
+                    equipo en la plataforma.
+                </a>
+            </p>
+            <b class="uppercase">Los demás agentes</b>
+            <p>
+                Deben unirse cuando reciban la invitación de su capitán.
+            </p>
         </div>
     </div>
 
@@ -26,3 +51,9 @@ use yii\helpers\Url;
     </div>
 
 </div>
+
+<style>
+.uppercase{
+    text-transform: uppercase;
+}
+</style>
