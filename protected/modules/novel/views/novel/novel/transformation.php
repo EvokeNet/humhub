9d1b4f9e9ffb5762b234528ce4035508a2b4842d
guilherme_fernandes <gfernandes@quanti.ca>
2016-08-16 15:55:57 -0300

    merge with dev

--- protected/modules/novel/views/novel/novel/transformation.php
+++ protected/modules/novel/views/novel/novel/transformation.php
@@ -9,6 +9,7 @@ use app\modules\novel\models\NovelPage;
   <div class="col-md-8 col-md-offset-2">
     <div class="panel panel-default">
       <div class="panel-body text-center">
+        <img src="<?php echo Url::to('@web/themes/Evoke/img/alchemy.png') ?>" width = "120px" style = "border-radius: 50%; border: 3px solid #254054; margin-bottom:10px">
         <h4><?php echo Yii::t('NovelModule.base', "As you see, Marta's story continues. She, like you, is committed to transforming her community. That's why you have {points} points in the power of Transformation.<br><br>You have always had this power. Now it is time to discover your other hidden powers.", array('points' => $points)) ?></h4>
         <br>
         <?php echo Html::a(
