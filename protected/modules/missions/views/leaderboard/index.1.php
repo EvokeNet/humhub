e5fed5c17ed01af6ef08cd0c2dfbb44a662c6b93
Renata Japur <rjapur@quanti.ca>
2016-08-27 13:36:08 -0300

    color changes

--- protected/modules/missions/views/leaderboard/index.1.php
+++ protected/modules/missions/views/leaderboard/index.1.php
@@ -13,18 +13,12 @@ $this->pageTitle = Yii::t('MissionsModule.base', 'Leaderboard');
 <div class="container">
     <div class="row">
         <div class="col-sm-8">
-            
-            <!--<div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 style = "padding: 20px; font-weight:bold"><?php echo Yii::t('MissionsModule.leaderboard', 'Leaderboard') ?></h3>
-                </div>
-            </div>-->
-            
-            <!--<h3 style = "padding: 20px; font-weight:bold"><?php echo Yii::t('MissionsModule.leaderboard', 'Leaderboard') ?></h3>-->
-            
-            <!-- Top Teams By Evidences Submitted -->
-            
+
             <div class="panel panel-default">
+
+                <h3 style = "padding: 20px; font-weight:bold"><?php echo Yii::t('MissionsModule.leaderboard', 'Leaderboard') ?></h3>
+
+                <!-- Top Teams By Evidences Submitted -->
                 <div class="panel-heading">
                     <h4><?php echo Yii::t('MissionsModule.leaderboard', 'Top Teams By Evidences Submitted') ?></h4>
                 </div>
@@ -33,18 +27,20 @@ $this->pageTitle = Yii::t('MissionsModule.base', 'Leaderboard');
                     <div class="row">
                         <?php foreach($ranking['rank_teams_evidences'] as $key => $r): ?>
 
-                            <div class = "grey-box" style = "padding: 15px 20px; margin-bottom:15px">
+                            <div class="col-sm-6">
+                                <div class = "grey-box" style = "padding: 15px 20px; margin-bottom:15px">
                                     
-                                <div class="row">
-                                    <div class="col-sm-9">
-                                        <span style = "font-size: 14pt; font-weight: 700; color: #254054;"><?php echo $key + 1; ?>.</span>&nbsp;&nbsp;
-                                        <?= Html::a($r['name'], ['/space/space', 'sguid' => $r['guid']], ['style' => 'font-size: 14pt; font-weight: 700; color: #2273AC;']) ?>
+                                    <div class="row">
+                                        <div class="col-sm-9">
+                                            <span style = "font-size: 14pt; font-weight: 700; color: #254054;"><?php echo $key + 1; ?>.</span>&nbsp;&nbsp;
+                                            <?= Html::a($r['name'], ['/space/space', 'sguid' => $r['guid']], ['style' => 'font-size: 14pt; font-weight: 700; color: #2273AC;']) ?>
+                                        </div>
+                                        <div class="col-sm-3">
+                                            <span style = "float:right; font-size: 14pt; color: #3399E1; font-weight: 700;"><?php echo $r['evidences']; ?></span>
+                                        </div>
                                     </div>
-                                    <div class="col-sm-3">
-                                        <span style = "float:right; font-size: 14pt; color: #3399E1; font-weight: 700;"><?php echo $r['evidences']; ?></span>
-                                    </div>
+                                    
                                 </div>
-                                
                             </div>
 
                         <?php endforeach; ?>
@@ -57,11 +53,8 @@ $this->pageTitle = Yii::t('MissionsModule.base', 'Leaderboard');
                         <span style = "font-size: 12pt; font-weight: 700; color: #254054;"><?php echo Yii::t('MissionsModule.leaderboard', "My Team's Evidences Submitted: {evidences}", array('evidences' => $ranking['my_team_evidences']['evidences'])); ?></span>
                     <?php endif; ?>
                 </div>
-            </div>
-            
-            <!-- Top Teams By Reviews Given -->
-            
-            <div class="panel panel-default">
+
+                <!-- Top Teams By Reviews Given -->
                 <div class="panel-heading">
                     <h4><?php echo Yii::t('MissionsModule.leaderboard', 'Top Teams By Reviews Given') ?></h4>
                 </div>
@@ -70,18 +63,20 @@ $this->pageTitle = Yii::t('MissionsModule.base', 'Leaderboard');
                     <div class="row">
                         <?php foreach($ranking['rank_teams_reviews'] as $key => $r): ?>
 
-                            <div class = "grey-box" style = "padding: 15px 20px; margin-bottom:15px">
+                            <div class="col-sm-6">
+                                <div class = "grey-box" style = "padding: 15px 20px; margin-bottom:15px">
                                     
-                                <div class="row">
-                                    <div class="col-sm-9">
-                                        <span style = "font-size: 14pt; font-weight: 700; color: #254054;"><?php echo $key + 1; ?>.</span>&nbsp;&nbsp;
-                                        <?= Html::a($r['name'], ['/space/space', 'sguid' => $r['guid']], ['style' => 'font-size: 14pt; font-weight: 700; color: #2273AC;']) ?>
+                                    <div class="row">
+                                        <div class="col-sm-9">
+                                            <span style = "font-size: 14pt; font-weight: 700; color: #254054;"><?php echo $key + 1; ?>.</span>&nbsp;&nbsp;
+                                            <?= Html::a($r['name'], ['/space/space', 'sguid' => $r['guid']], ['style' => 'font-size: 14pt; font-weight: 700; color: #2273AC;']) ?>
+                                        </div>
+                                        <div class="col-sm-3">
+                                            <span style = "float:right; font-size: 14pt; color: #3399E1; font-weight: 700;"><?php echo $r['reviews']; ?></span>
+                                        </div>
                                     </div>
-                                    <div class="col-sm-3">
-                                        <span style = "float:right; font-size: 14pt; color: #3399E1; font-weight: 700;"><?php echo $r['reviews']; ?></span>
-                                    </div>
+                                    
                                 </div>
-                                
                             </div>
 
                         <?php endforeach; ?>
@@ -94,11 +89,8 @@ $this->pageTitle = Yii::t('MissionsModule.base', 'Leaderboard');
                         <span style = "font-size: 12pt; font-weight: 700; color: #254054;"><?php echo Yii::t('MissionsModule.leaderboard', "My Team's Reviews Submitted: {evidences}", array('evidences' => $ranking['my_team_reviews']['reviews'])); ?></span>
                     <?php endif; ?>
                 </div>
-            </div>
-            
-            <!-- Top Agents By Evidences Submitted -->
-            
-            <div class="panel panel-default">
+
+                <!-- Top Agents By Evidences Submitted -->
                 <div class="panel-heading">
                     <h4><?php echo Yii::t('MissionsModule.leaderboard', 'Top Agents By Evidences Submitted') ?></h4>
                 </div>
@@ -107,18 +99,20 @@ $this->pageTitle = Yii::t('MissionsModule.base', 'Leaderboard');
                     <div class="row">
                         <?php foreach($ranking['rank_agents_evidences'] as $key => $r): ?>
 
-                        <div class = "grey-box" style = "padding: 15px 20px; margin-bottom:15px">
+                        <div class="col-sm-6">
+                            <div class = "grey-box" style = "padding: 15px 20px; margin-bottom:15px">
                                 
-                            <div class="row">
-                                <div class="col-sm-9">
-                                    <span style = "font-size: 14pt; font-weight: 700; color: #254054;"><?php echo $key + 1; ?>.</span>&nbsp;&nbsp;
-                                    <?= Html::a($r['username'], ['/user/profile', 'uguid' => $r['guid']], ['style' => 'font-size: 14pt; font-weight: 700; color: #2273AC;']) ?>
+                                <div class="row">
+                                    <div class="col-sm-9">
+                                        <span style = "font-size: 14pt; font-weight: 700; color: #254054;"><?php echo $key + 1; ?>.</span>&nbsp;&nbsp;
+                                        <?= Html::a($r['username'], ['/user/profile', 'uguid' => $r['guid']], ['style' => 'font-size: 14pt; font-weight: 700; color: #2273AC;']) ?>
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <span style = "float:right; font-size: 14pt; color: #3399E1; font-weight: 700;"><?php echo $r['evidences']; ?></span>
+                                    </div>
                                 </div>
-                                <div class="col-sm-3">
-                                    <span style = "float:right; font-size: 14pt; color: #3399E1; font-weight: 700;"><?php echo $r['evidences']; ?></span>
-                                </div>
+                                
                             </div>
-                            
                         </div>
 
                         <?php endforeach; ?>
@@ -131,11 +125,8 @@ $this->pageTitle = Yii::t('MissionsModule.base', 'Leaderboard');
                         <span style = "font-size: 12pt; font-weight: 700; color: #254054;"><?php echo Yii::t('MissionsModule.leaderboard', 'My Evidences Submitted: {evidences}', array('evidences' => $ranking['my_evidences']['evidences'])); ?></span>
                     <?php endif; ?>
                 </div>
-            </div>
-            
-            <!-- Top Agents By Reviews Given -->
-            
-            <div class="panel panel-default">
+
+                <!-- Top Agents By Reviews Given -->
                 <div class="panel-heading">
                     <h4><?php echo Yii::t('MissionsModule.leaderboard', 'Top Agents By Reviews Given') ?></h4>
                 </div>
@@ -144,18 +135,20 @@ $this->pageTitle = Yii::t('MissionsModule.base', 'Leaderboard');
                     <div class="row">
                         <?php foreach($ranking['rank_agents_reviews'] as $key => $r): ?>
 
-                        <div class = "grey-box" style = "padding: 15px 20px; margin-bottom:15px">
+                        <div class="col-sm-6">
+                            <div class = "grey-box" style = "padding: 15px 20px; margin-bottom:15px">
                                 
-                            <div class="row">
-                                <div class="col-sm-9">
-                                    <span style = "font-size: 14pt; font-weight: 700; color: #254054;"><?php echo $key + 1; ?>.</span>&nbsp;&nbsp;
-                                    <?= Html::a($r['username'], ['/user/profile', 'uguid' => $r['guid']], ['style' => 'font-size: 14pt; font-weight: 700; color: #2273AC;']) ?>
+                                <div class="row">
+                                    <div class="col-sm-9">
+                                        <span style = "font-size: 14pt; font-weight: 700; color: #254054;"><?php echo $key + 1; ?>.</span>&nbsp;&nbsp;
+                                        <?= Html::a($r['username'], ['/user/profile', 'uguid' => $r['guid']], ['style' => 'font-size: 14pt; font-weight: 700; color: #2273AC;']) ?>
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <span style = "float:right; font-size: 14pt; color: #3399E1; font-weight: 700;"><?php echo $r['reviews']; ?></span>
+                                    </div>
                                 </div>
-                                <div class="col-sm-3">
-                                    <span style = "float:right; font-size: 14pt; color: #3399E1; font-weight: 700;"><?php echo $r['reviews']; ?></span>
-                                </div>
+                                
                             </div>
-                            
                         </div>
 
                         <?php endforeach; ?>
@@ -170,11 +163,8 @@ $this->pageTitle = Yii::t('MissionsModule.base', 'Leaderboard');
                       <?php //echo Yii::t('MissionsModule.leaderboard', 'My Team Reviews Submitted: {evidences}', array('evidences' => $ranking['my_reviews']['reviews'])); ?>
                     <?php endif; ?>
                 </div>
-            </div>
-            
-            <!-- Top Mentors By Reviews Given -->
-            
-            <div class="panel panel-default">
+
+                <!-- Top Mentors By Reviews Given -->
                 <div class="panel-heading">
                     <h4><?php echo Yii::t('MissionsModule.leaderboard', 'Top Mentors By Reviews Given') ?></h4>
                 </div>
@@ -183,18 +173,20 @@ $this->pageTitle = Yii::t('MissionsModule.base', 'Leaderboard');
                     <div class="row">
                         <?php foreach($ranking['rank_mentors_reviews'] as $key => $r): ?>
 
-                        <div class = "grey-box" style = "padding: 15px 20px; margin-bottom:15px">
+                        <div class="col-sm-6">
+                            <div class = "grey-box" style = "padding: 15px 20px; margin-bottom:15px">
                                 
-                            <div class="row">
-                                <div class="col-sm-9">
-                                    <span style = "font-size: 14pt; font-weight: 700; color: #254054;"><?php echo $key + 1; ?>.</span>&nbsp;&nbsp;
-                                    <?= Html::a($r['username'], ['/user/profile', 'uguid' => $r['guid']], ['style' => 'font-size: 14pt; font-weight: 700; color: #2273AC;']) ?>
+                                <div class="row">
+                                    <div class="col-sm-9">
+                                        <span style = "font-size: 14pt; font-weight: 700; color: #254054;"><?php echo $key + 1; ?>.</span>&nbsp;&nbsp;
+                                        <?= Html::a($r['username'], ['/user/profile', 'uguid' => $r['guid']], ['style' => 'font-size: 14pt; font-weight: 700; color: #2273AC;']) ?>
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <span style = "float:right; font-size: 14pt; color: #3399E1; font-weight: 700;"><?php echo $r['reviews']; ?></span>
+                                    </div>
                                 </div>
-                                <div class="col-sm-3">
-                                    <span style = "float:right; font-size: 14pt; color: #3399E1; font-weight: 700;"><?php echo $r['reviews']; ?></span>
-                                </div>
+                                
                             </div>
-                            
                         </div>
 
                         <?php endforeach; ?>
@@ -210,7 +202,7 @@ $this->pageTitle = Yii::t('MissionsModule.base', 'Leaderboard');
                     <?php endif; ?>
                 </div>
             </div>
-                
+
         </div>
 
         <div class="col-sm-4">
