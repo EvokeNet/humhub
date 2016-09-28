b8942f830ce176e76cc1a12230ef1cca6fe5128b
Jayant Gandhi <jayant.gandhi127@gmail.com>
2016-09-28 17:25:52 -0400

    record user type on vote save

--- protected/modules/missions/controllers/EvidenceController.php
+++ protected/modules/missions/controllers/EvidenceController.php
@@ -389,6 +389,7 @@ class EvidenceController extends ContentContainerController
                 $vote->comment = $comment;
                 $vote->flag = $flag;
                 $vote->value = $grade;
+                $vote->user_type = $user->group->name;
                 $vote->save();
                 $evocoin_earned = 0;
 
