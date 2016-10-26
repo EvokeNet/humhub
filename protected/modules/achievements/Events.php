223f011e6638913c3289df5b20645d8f4f48bbfa
Guihrmsfo <guilherme_oliveira55@hotmail.com>
2016-10-26 17:40:54 -0200

    achievements: event after save started

--- protected/modules/achievements/Events.php
+++ protected/modules/achievements/Events.php
@@ -4,6 +4,7 @@ namespace humhub\modules\achievements;
 
 use Yii;
 use yii\helpers\Url;
+use app\modules\missions\models\Evidence;
 // use humhub\modules\matching_questions\models\MatchingQuestions;
 
 /**
@@ -12,6 +13,19 @@ use yii\helpers\Url;
  */
 class Events extends \yii\base\Object
 {
+
+    public static function onEvidenceAfterSave($event){
+
+        //get user id (evidence's author)
+
+        $user_id = $event->sender->created_by;
+
+        $evidences_count = Evidence::find()
+        ->join('INNER JOIN', 'content as c', '`c`.`object_model`=\''.str_replace("\\", "\\\\", Evidence::classname()).'\' AND `evidence`.`id` = `c`.`object_id`')
+        ->where(['evidence.created_by' => $this->user->id])
+        ->andWhere(['visibility' => 1])
+        ->count();
+    }
 
     public static function onAdminMenuInit($event)
     {
--- protected/modules/achievements/config.php
+++ protected/modules/achievements/config.php
@@ -5,16 +5,20 @@
     use humhub\widgets\BaseMenu;
     use humhub\widgets\TopMenu;
     use humhub\modules\space\widgets\Menu;
+    use app\modules\missions\models\Evidence;
 
     return [
         'id' => 'achievements',
         'class' => 'humhub\modules\achievements\Module',
         'namespace' => 'humhub\modules\achievements',
         'events' => [
-            ['class' => \humhub\modules\admin\widgets\AdminMenu::className(), 'event' => AdminMenu::EVENT_INIT, 'callback' => ['humhub\modules\achievements\Events', 'onAdminMenuInit']],
-            ['class' => Menu::className(), 'event' => Menu::EVENT_INIT, 'callback' => ['\humhub\modules\achievements\Events', 'onSpaceMenuInit']],
+            //['class' => \humhub\modules\admin\widgets\AdminMenu::className(), 'event' => AdminMenu::EVENT_INIT, 'callback' => ['humhub\modules\achievements\Events', 'onAdminMenuInit']],
+            //['class' => Menu::className(), 'event' => Menu::EVENT_INIT, 'callback' => ['\humhub\modules\achievements\Events', 'onSpaceMenuInit']],
+            ['class' => Evidence::className(), 'event' => Evidence::EVENT_AFTER_INSERT, 'callback' => ['\humhub\modules\achievements\Events', 'onEvidenceAfterSave']],
+            ['class' => Evidence::className(), 'event' => Evidence::EVENT_AFTER_UPDATE, 'callback' => ['\humhub\modules\achievements\Events', 'onEvidenceAfterSave']],
         ],
 
     ];
 
 ?>
+
