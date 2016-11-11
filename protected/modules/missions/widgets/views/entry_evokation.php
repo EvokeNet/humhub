53af52577de6298fb23032c2daa281dc43be7bb8
Guihrmsfo <guilherme_oliveira55@hotmail.com>
2016-11-11 16:06:20 -0200

    mentors can invest on evokations

--- protected/modules/missions/widgets/views/entry_evokation.php
+++ protected/modules/missions/widgets/views/entry_evokation.php
@@ -58,7 +58,7 @@ $youtube_code = $evokation->youtube_url ? $evokation->getYouTubeCode($evokation-
 
 <br>
 
-<?php if(!$evokation_investment && $evokation->content->user_id != Yii::$app->user->getIdentity()->id && $user->group->name != "Mentors"): ?>
+<?php if(!$evokation_investment && $evokation->content->user_id): ?>
 <div>
     
     <!-- DISABLED
