48c9cc35b6bf54e05ac10c64f16475ec7819ad5f
Guihrmsfo <guilherme_oliveira55@hotmail.com>
2016-12-13 17:51:10 -0200

    gift evocoin working

--- protected/modules/missions/controllers/GiftController.php
+++ protected/modules/missions/controllers/GiftController.php
@@ -7,15 +7,24 @@ use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use humhub\modules\missions\controllers\AlertController;
 use app\modules\coin\models\Wallet;
+use humhub\modules\user\models\User;
 
 class GiftController extends Controller
 {
 	public function actionEvocoins(){
 		$user = Yii::$app->user->getIdentity();
         $wallet = Wallet::findOne(['owner_id' => $user->id]);
+        $receiver_id = Yii::$app->request->get('user_id');
+        $receiver = User::findOne($receiver_id);
+        $receiver_wallet = Wallet::findOne(['owner_id' => $receiver_id]);
         $value = Yii::$app->request->post('Gift')['evocoins'];
-		if($wallet->amount >= $value){
-			AlertController::createAlert("Success!", Yii::t('MissionsModule.base', 'You gave it away some of your evocoins.'));
+
+        if(($wallet->amount >= $value) && ($value > 0 )){
+			$wallet->amount -= $value;
+			$wallet->save();
+			$receiver_wallet->amount += $value;
+			$receiver_wallet->save();
+			AlertController::createAlert("Success!", Yii::t('MissionsModule.base', 'You gave {number} evocoins to {name}.', ['number' => $value, 'name' => $receiver->username]));
 		}else{
 			AlertController::createAlert("Error", Yii::t('MissionsModule.base', 'No enough Evocoins!'));
 		}
