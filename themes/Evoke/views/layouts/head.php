4de4a65486a86a2a73de751a2f077af8bbcfff0d
Jayant Gandhi <jayant.gandhi127@gmail.com>
2017-01-26 21:28:54 -0500

    get tween

--- themes/Evoke/views/layouts/head.php
+++ themes/Evoke/views/layouts/head.php
@@ -2,3 +2,7 @@
 <link href="<?php echo $this->theme->getBaseUrl() . '/font/open_sans/open-sans.css'; ?>" rel="stylesheet">
 <script type="text/javascript" src="<?php echo $this->theme->getBaseUrl() . '/js/char-counter.js'; ?>"></script>
 <script type="text/javascript" src="<?php echo $this->theme->getBaseUrl() . '/js/products.js'; ?>"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/TweenLite.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/TimelineLite.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/jquery.gsap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/plugins/CSSPlugin.min.js"></script>
