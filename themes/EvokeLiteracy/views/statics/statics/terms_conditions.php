846db61e28a9e949fa273e7b6b582c2436f255b8
Renata Japur <rjapur@quanti.ca>
2017-02-03 18:24:06 -0200

    literacy texts

---
+++ themes/EvokeLiteracy/views/statics/statics/terms_conditions.php
@@ -0,0 +1,30 @@
+<div class="container">
+      
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4><?php echo Yii::t('StaticsModule.base', 'Terms and Conditions of Use & Privacy Policy') ?></h4>
+            </div>
+            <div class="panel-body">
+                <p>
+
+                Terms and Conditions of Use & Privacy Policy<br><br>
+                
+                As a member of the Evoke Literacy community (whether a mentor or agent), I understand and agree to the following:<br><br>
+                I will be responsible and ethical in my interactions with children and other vulnerable populations.<br>
+                I will work to protect these populations from getting hurt.<br>
+                I will ensure that the children and guardians know they have a choice to participate.<br>
+                I will respect the culture of the individuals with which I interact.<br>
+                I will be sure to speak clearly and in a language easily understood by children and adults.<br>
+                I will not make promises that can’t be kept.<br>
+                I will respect the privacy of the children and other populations, always making sure to ask before recording what is spoken or taking pictures/video.<br>
+                I will keep identities secret and respect the wishes of the participants.<br>
+                I will act appropriately when working with others, respecting their space, time, and needs.<br>
+                If during my Evoke Literacy work, I come in contact with a child or person who is in need of help-then I will report the issue to the proper authorities.<br>
+                By clicking the agreement link, I understand that it is my responsibility to ensure the safety of children and vulnerable populations I come in contact through my work on Evoke Literacy.<br>
+
+                </p>
+
+                </div>
+        </div>
+</div>   
+ 
