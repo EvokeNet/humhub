0329f9f5dcaf808f4e897bc9b9e56a9cf4e83a87
Renata Japur <rjapur@quanti.ca>
2017-02-08 06:13:29 -0200

    activity stream overflow fix

--- themes/EvokeDark/views/activity/layouts/web.php
+++ themes/EvokeDark/views/activity/layouts/web.php
@@ -8,12 +8,12 @@ use humhub\modules\space\activities\MemberAdded;
 <?php elseif ($clickable): ?>
     <a href="#" onClick="activityShowItem(<?= $record->id; ?>); return false;">
 <?php endif; ?>
-    <li class="activity-entry">
-        <div class="media">
+    <li class="activity-entry" style="padding: 15px;">
+        <div class="media" style="overflow:visible">
 
             <div class="media-body">
 
-                <div style="">
+                <div style="top: 50%; transform: translateY(-50%); position: absolute;">
                     <?php if ($originator !== null) : ?>
                         <!-- Show user image -->
                         <img class="media-object img-rounded pull-left" data-src="holder.js/32x32" alt="32x32"
@@ -23,10 +23,7 @@ use humhub\modules\space\activities\MemberAdded;
                 </div>
 
                 <!-- Show content -->
-                <div style="top: 50%;
-    transform: translateY(-50%);
-    position: absolute;
-    left: 10%;"><?php echo $content; ?></div><br/>
+                <div style="top: 50%; transform: translateY(-50%); position: absolute; left: 10%;"><?php echo $content; ?></div><br/>
 
                 <!-- show time -->
                 <?php //echo \humhub\widgets\TimeAgo::widget(['timestamp' => $record->content->created_at]); ?>
