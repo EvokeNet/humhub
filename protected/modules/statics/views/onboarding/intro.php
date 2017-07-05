3393df80aed210f530a9eb76348514db6cb8de48
Renata Japur <rjapur@quanti.ca>
2017-07-04 23:43:10 -0300

    content for onboarding

---
+++ protected/modules/statics/views/onboarding/intro.php
@@ -0,0 +1,17 @@
+<?php
+
+use yii\helpers\Html;
+
+?>
+
+<h4 style="background-color: #101C2A; text-align: center; padding: 10px 0; margin: 0px 0 20px; color: #5aa2c6;"><?php echo Yii::t('StaticsModule.base', 'INTRODUCTION') ?></h4>
+
+<div style="text-align:center; margin: 20px 0">
+	<img src="https://about.canva.com/wp-content/uploads/sites/3/2015/01/timeline_infographic.png" />
+</div>
+
+<div style="text-align:right; margin: 10px 40px">
+<?php echo Html::a(
+    Yii::t('StaticsModule.base', 'Next'),
+    ['/statics/onboarding/video'], array('class' => 'btn btn-lg btn-cta1', 'style' => 'padding: 0 80px')); ?>
+</div>
