2a5df75e996794be96130917bfe0c4568ec10b92
Renata Japur <rjapur@quanti.ca>
2018-03-02 00:20:19 -0300

    quiz questions start

---
+++ protected/modules/missions/migrations/m180215_034107_quiz_questions.php
@@ -0,0 +1,47 @@
+<?php
+
+use yii\db\Migration;
+
+class m180215_034107_quiz_questions extends Migration
+{
+    public function up()
+    {
+        $this->createTable('quiz_questions', array(
+            'id' => 'pk',
+            'question_headline' => 'text NOT NULL',
+            'power_id' => 'int(11) NOT NULL',
+            'level_id' => 'int(11) NOT NULL',
+                ), '');
+
+        $this->addForeignKey(
+            'fk-quiz_questions-power_id',
+            'quiz_questions',
+            'power_id',
+            'powers',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    public function down()
+    {
+        echo "m180215_034107_quiz_questions cannot be reverted.\n";
+
+        $this->dropForeignKey('fk-quiz_questions-power_id', 'quiz_questions');
+        
+        $this->dropTable('quiz_questions');
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
---
+++ protected/modules/missions/migrations/m180215_034128_quiz_question_answers.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\db\Migration;
+
+class m180215_034128_quiz_question_answers extends Migration
+{
+    public function up()
+    {
+        $this->createTable('quiz_question_answers', array(
+            'id' => 'pk',
+            'answer_headline' => 'text NOT NULL',
+            'quiz_question_id' => 'int(11) NOT NULL',
+                ), '');
+
+        $this->addForeignKey(
+            'fk-quiz_question_answers-quiz_question_id',
+            'quiz_question_answers',
+            'quiz_question_id',
+            'quiz_questions',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    public function down()
+    {
+        echo "m180215_034128_quiz_question_answers cannot be reverted.\n";
+
+        $this->dropForeignKey('fk-quiz_question_answers-quiz_question_id', 'quiz_question_answers');
+        
+        $this->dropTable('quiz_question_answers');
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
