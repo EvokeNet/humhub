6e79072e3526d5031d5f21346e3b19ce5d61daff
Renata Japur <rjapur@quanti.ca>
2018-03-20 19:14:15 -0300

    gtm code

--- themes/EvokeLiteracy/views/layouts/main.php
+++ themes/EvokeLiteracy/views/layouts/main.php
@@ -88,9 +88,9 @@ AppAsset::register($this);
     <?php $this->beginBody() ?>
 
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5NTGH48"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5NTGH48"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) --> 
 
     <?php //include_once("analyticstracking.php") ?>
 
